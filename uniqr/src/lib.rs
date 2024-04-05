1da49aa0b7bbdbfd4829bae535fb3905978eea1d
izumix03 <takahiro.izumikawa@gmail.com>
2024-04-05 18:48:03 +0900

    Add writing functionality and line counting to uniqr program

--- uniqr/src/lib.rs
+++ uniqr/src/lib.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 use std::fs::File;
 use std::io;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Write};
 use clap::{App, Arg};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
@@ -41,7 +41,7 @@ pub fn get_args() -> MyResult<Config> {
         )
         .get_matches();
 
-    Ok(Config{
+    Ok(Config {
         in_file: matches.value_of_lossy("in_file").unwrap().to_string(),
         out_file: matches.value_of_lossy("out_file").map(String::from),
         count: matches.is_present("count"),
@@ -52,14 +52,43 @@ pub fn run(config: Config) -> MyResult<()> {
     let mut file = open(&config.in_file)
         .map_err(|e| format!("{}: {}", config.in_file, e))?;
     let mut line = String::new();
+    let mut previous = String::new();
+    let mut count: u64 = 0;
+    let mut out_file: Box<dyn Write> = match &config.out_file {
+        Some(out_name) => Box::new(File::create(out_name)?),
+        _ => Box::new(io::stdout()),
+    };
+
+    // out_file が渡されるから mut にしないといけない
+    let mut print = |count: u64, text: &str| -> MyResult<()> {
+        if count > 0 {
+            if config.count {
+                // 所有権を束縛してくれて安全！
+                write!(out_file, "{:>4} {}", count, text)?;
+            } else {
+                write!(out_file, "{}", text)?;
+            }
+        }
+        Ok(())
+    };
 
     loop {
         let bytes = file.read_line(&mut line)?;
         if bytes == 0 {
             break;
         }
-        print!("{}", line);
+
+        if line.trim_end() != previous.trim_end() {
+            print(count, &previous)?;
+            previous = line.clone();
+            count = 0;
+        }
+        count += 1;
         line.clear();
+    }
+
+    if count > 0 {
+        print(count, &previous)?;
     }
 
     Ok(())
